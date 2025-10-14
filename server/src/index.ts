11af85e7ae6cc12e27eac6bb0f7f09902118ad39
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-10-15 00:07:04 +0530

    server-client-with-getstream-credentials

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import { apikey } from "./serverClient";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "AI Writing Assistant server is running",
+    apikey: apikey,
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running at https://localhost:${port}`);
+});
